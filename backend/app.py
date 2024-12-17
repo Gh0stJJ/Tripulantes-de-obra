cbdb6071cef17ad22a0d6914bc9eaa93e27f4bf6
Juan Valdiviezo <valdiviezojuan7@gmail.com>
2024-12-16 21:23:53 -0500

    feat: Agregar rutas de inicio de sesión, registro y problemas; actualizar enlaces en las plantillas

--- backend/app.py
+++ backend/app.py
@@ -36,6 +36,18 @@ def view_reviews():
 def welcome():
     return render_template('welcome.html')
 
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template('register.html')
+
+@app.route('/problems', methods=['GET'])
+def problems():
+    return render_template('problems.html')
+
 
 if __name__ == '__main__':
     #csrf.init_app(app)
